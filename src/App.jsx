30464c9346f5a9ab7fee9868013860451de34a17
bhaskar36 <bhaskar.chagge@gmail.com>
2025-07-21 15:05:07 +0530

    Update App.jsx and add .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>this is my static website which will be deployed</p>
+      <p>This project has been deployed using devops</p>
     </>
   )
 }
